253500cd7d9ad99e1b6c004a60362151fa949247
Couassi-D <couassialex@gmail.com>
2026-09-18 21:42:11 -0400

    Remove git commands from paycheck.py

--- paycheck.py
+++ paycheck.py
@@ -37,5 +37,3 @@ print(f'Net pay: ${net_pay:.2f}')
 # Chapters 1 and 2 only. Use variables, input(), arithmetic, type conversion,
 # and print(). Do not use if statements, loops, functions, or imports.
 # Your code runs top to bottom, once.
-git add paycheck.py
-git push 
